d9032e1c88c5a527d39dfb49688eda0226ea1345
francesco-vaselli <71897901+francesco-vaselli@users.noreply.github.com>
2021-02-21 22:54:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../cmepda_vaselli'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
